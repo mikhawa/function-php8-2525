cd3f6e168128e52f1a2aee344ccb17a91d1a3a7a
mikhawa <michaeljpitz@gmail.com>
2025-01-16 14:44:17 +0100

    css and js

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,16 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\PhpFunction;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 final class HomepageController extends AbstractController{
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
+
+        $listephps = $em->getRepository(PhpFunction::class)
+                     ->findBy([],['title'=>"ASC"]);
+
         return $this->render('homepage/index.html.twig', [
-            'controller_name' => 'HomepageController',
+            'liste' => $listephps,
         ]);
     }
+
 }
